5cba74c26bf0c45a6939158e60fd63a43dfa7209
davidkarelh <kareldavid9@gmail.com>
2023-01-27 08:37:10 +0700

    Add HTML template

--- server.go
+++ server.go
@@ -10,10 +10,19 @@ import (
 func main() {
 	fmt.Println("Hello World!")
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.html")
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello World!",
 		})
 	})
+
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "GGWP",
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
