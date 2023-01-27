479ac7798d312a92d960499599085cbb08b3159a
davidkarelh <kareldavid9@gmail.com>
2023-01-27 17:47:11 +0700

    Add link to each encrypt/decrypt methods page

--- controllers/controller.go
+++ controllers/controller.go
@@ -10,6 +10,12 @@ type IController interface {
 	HelloWorld(ctx *gin.Context)
 	GGWP(ctx *gin.Context)
 	Index(ctx *gin.Context)
+	Vignere(ctx *gin.Context)
+	AutoKeyVignere(ctx *gin.Context)
+	ExtendedVignere(ctx *gin.Context)
+	Affine(ctx *gin.Context)
+	Playfair(ctx *gin.Context)
+	Hill(ctx *gin.Context)
 }
 
 type Controller struct {
@@ -35,5 +41,41 @@ func (c *Controller) GGWP(ctx *gin.Context) {
 func (c *Controller) Index(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "home/index.html", gin.H{
 		"title": "Home",
+	})
+}
+
+func (c *Controller) Vignere(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "encrypt-decrypt/vignere.html", gin.H{
+		"title": "Vignere Cipher",
+	})
+}
+
+func (c *Controller) AutoKeyVignere(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "encrypt-decrypt/auto-key-vignere.html", gin.H{
+		"title": "Auto-key Vignere Cipher",
+	})
+}
+
+func (c *Controller) ExtendedVignere(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "encrypt-decrypt/extended-vignere.html", gin.H{
+		"title": "Extended Vignere Cipher",
+	})
+}
+
+func (c *Controller) Affine(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "encrypt-decrypt/affine.html", gin.H{
+		"title": "Affine Cipher",
+	})
+}
+
+func (c *Controller) Playfair(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "encrypt-decrypt/playfair.html", gin.H{
+		"title": "Playfair Cipher",
+	})
+}
+
+func (c *Controller) Hill(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "encrypt-decrypt/hill.html", gin.H{
+		"title": "Hill Cipher",
 	})
 }
--- server.go
+++ server.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/**/*.html")
+	r.Static("/assets", "./assets")
 
 	var (
 		controller controllers.IController = controllers.NewController()
@@ -22,6 +23,18 @@ func main() {
 	r.GET("/ggwp", func(c *gin.Context) { controller.GGWP(c) })
 
 	r.GET("/", func(c *gin.Context) { controller.Index(c) })
+
+	r.GET("/vignere", func(c *gin.Context) { controller.Vignere(c) })
+
+	r.GET("/auto-key-vignere", func(c *gin.Context) { controller.AutoKeyVignere(c) })
+
+	r.GET("/extended-vignere", func(c *gin.Context) { controller.ExtendedVignere(c) })
+
+	r.GET("/affine", func(c *gin.Context) { controller.Affine(c) })
+
+	r.GET("/playfair", func(c *gin.Context) { controller.Playfair(c) })
+
+	r.GET("/hill", func(c *gin.Context) { controller.Hill(c) })
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
