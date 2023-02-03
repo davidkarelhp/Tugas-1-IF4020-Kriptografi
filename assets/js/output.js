4dde389ea9fb97cbeb9755ed8de5d8ba430a7b40
davidkarelh <kareldavid9@gmail.com>
2023-02-03 17:57:14 +0700

    Add download output text functionality

--- assets/js/output.js
+++ assets/js/output.js
@@ -3,12 +3,13 @@ let _5_letter_radio_label = document.querySelector("label[for='5-letter-radio']"
 let no_space_radio = document.getElementById("no-space-radio");
 let no_space_radio_label = document.querySelector("label[for='no-space-radio']");
 let result_area = document.getElementById("result-area");
+let download_output = document.getElementById("download-output");
 
 function chunkString(str, length) {
     return str.match(new RegExp('.{1,' + length + '}', 'g'));
 }
 
-_5_letter_radio.addEventListener("click", async (e) =>{
+_5_letter_radio.addEventListener("click", (e) =>{
     chunks = chunkString(result_area.value, 5);
     let newText = "";
     let l = chunks?.length ?? 0;
@@ -23,10 +24,19 @@ _5_letter_radio.addEventListener("click", async (e) =>{
     no_space_radio_label.classList.remove("disabled");
 });
 
-no_space_radio.addEventListener("click", async (e) =>{
+no_space_radio.addEventListener("click", (e) =>{
     result_area.value = result_area.value.replace(/\s+/g, '');
     _5_letter_radio_label.classList.remove("disabled");
     no_space_radio_label.classList.add("disabled");
+});
+
+download_output.addEventListener("click", (e) =>{
+    if (result_area.value == null || result_area.value == "") {
+        alert("Output Text is empty");
+    } else {
+        download("output.txt", result_area.value);
+    }
+    
 });
 
 function download(filename, text) {
